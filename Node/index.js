af5597cd6eca156669a1a261e84ed94778886a73
homelala <homelala@naver.com>
2022-03-29 16:18:34 +0900

    add: CRUD test

--- Node/index.js
+++ Node/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const app = express();
 const port = 3000;
 var users = [
   { id: 1, name: 'elice' },
   { id: 2, name: 'bak' },
   { id: 3, name: 'chris' },
 ];
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(morgan('dev'));
+
 app.get('/users', (req, res) => {
   req.query.limit = req.query.limit || 10;
   let limit = parseInt(req.query.limit, 10);
@@ -17,6 +20,51 @@ app.get('/users', (req, res) => {
   }
   res.json(users.slice(0, limit));
 });
+
+app.get('/users/:id', (req,res) => {
+  let id = parseInt(req.params.id,10);
+  if(Number.isNaN(id)) return res.status(400).end();
+
+  let user = users.filter((user) => user.id === id)[0];
+  if(!user) return res.status(404).end();
+  res.json(user);
+})
+
+app.delete('/users/:id', (req,res)=>{
+  let id = parseInt(req.params.id, 10);
+  if(Number.isNaN(id)) return res.status(400).end();
+  users.filter(user => user.id !== id);
+  res.status(204).end();
+})
+
+app.post('/users', (req,res)=>{
+  let name = req.body.name;
+  if(!name) return res.status(400).end();
+
+  let isConfict = users.filter(user => user.name === name).length
+  if(isConfict) return res.status(409).end();
+
+  let id = Date.now();
+  let user = {id, name};
+  users.push(user);
+  res.status(201).json(user);
+})
+
+app.put('/users/:id', (req,res)=>{
+  let id = parseInt(req.params.id, 10);
+  if(Number.isNaN(id)) return res.status(400).end();
+  
+  let name = req.body.name;
+  if(!name) return res.status(400).end();
+  let isConfict = users.filter(user => user.name === name).length
+  if(isConfict) return res.status(409).end();
+
+  let user = users.filter(user=>user.id === id)[0];
+  if(!user) return res.status(404).end();
+
+  user.name = name;
+  res.json(user);
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- Node/index.spec.js
+++ Node/index.spec.js
@@ -29,3 +29,135 @@ describe('GET /users는', () => {
     });
   });
 });
+
+describe('GET /users/1는?', ()=>{
+  describe('성공 시', () => {
+    it('id가 1인 유저 객체를 반환', (done) =>{
+      request(app)
+      .get('/users/1')
+      .end((err,res)=>{
+        res.body.should.have.property('id',1);
+        done();
+      })
+    })
+  })
+
+  describe('실패 시', () =>{
+    it('id가 숫자가 아닐 경우', (done) =>{
+      request(app)
+      .get('/users/one')
+      .expect(400)
+      .end(done);
+    });
+
+    it('id로 유저를 찾을 수 없을 경우', (done) => {
+      request(app)
+      .get('/users/999')
+      .expect(404)
+      .end(done);
+    })
+  })
+})
+
+describe('delete /users/1', ()=>{
+  describe('성공시', ()=>{
+    it('204를 응답한다.', (done)=>{
+      request(app)
+      .delete('/users/1')
+      .expect(204)
+      .end(done);
+    })
+  })
+
+  describe('실패 시', ()=>{
+    it('id가 숫자가 아닐경우 404로 응답', done => {
+      request(app)
+      .delete('/users/two')
+      .expect(400)
+      .end(done);
+    })
+  })
+})
+
+describe('POST /users', ()=>{
+  describe('성공 시', ()=>{
+    let body;
+    let name = 'daniel'
+    before(done => {
+      request(app)
+      .post('/users')
+      .send({name})
+      .expect(201)
+      .end((err,res)=>{
+        body = res.body;
+        done();
+      });
+    })
+    it('생성도딘 유저 객체를 반환한다.', () => {
+      body.should.have.property('id');
+    })
+    it('입력한 name을 반환한다.', ()=>{
+      body.should.have.property('name', name);
+    })
+  })
+  describe('실패 시', ()=>{
+    it('name 파라미터 누락시 400을 반환한다.', (done)=>{
+        request(app)
+        .post('/users')
+        .send({})
+        .expect(400)
+        .end(done)
+    })
+    it('name이 중복됬을 경우 400을 반환한다.', (done)=>{
+      request(app)
+      .post('/users')
+      .send({name:'elice'})
+      .expect(409)
+      .end(done)
+  })
+  })
+})
+
+describe('PUT /user/:id', ()=>{
+  describe('성공 시', ()=>{
+    it('변경된 name을 응답한다.', (done)=>{
+      const name = 'den';
+      request(app)
+      .put('/users/3')
+      .send({name})
+      .end((err,res)=>{
+        res.body.should.have.property('name',name);
+        done();
+      })
+    })
+  })
+  describe('실패 시', ()=>{
+    it('정수가 아닌 id일 경우 400을 응답한다.', done => {
+      request(app)
+      .put('/users/one')
+      .expect(400)
+      .end(done);
+    })
+    it('name이 없을 경우 400을 응답한다.', done => {
+      request(app)
+      .put('/users/1')
+      .send({})
+      .expect(400)
+      .end(done);
+    });
+    it('없는 유저일 경우 404를 응답한다.', done => {
+      request(app)
+      .put('/users/999')
+      .send({name:'foo'})
+      .expect(404)
+      .end(done);
+    });
+    it('이름 이 중복일 경우 409를 응답한다.', done => {
+      request(app)
+      .put('/users/3')
+      .send({name:'bak'})
+      .expect(409)
+      .end(done);
+    });
+  })
+})
